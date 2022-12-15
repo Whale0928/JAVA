a69c92e18a2f04444b686499c3e221649c356fff
whale0928 <rlagusrl928@gmail.com>
2022-12-15 22:26:20 +0900

    feat : Create Vo Object

--- src/main/java/vo/For_Equals_And_HashCode_Vo.java
+++ src/main/java/vo/For_Equals_And_HashCode_Vo.java
@@ -1,6 +1,9 @@
 package vo;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor @Getter
 public class For_Equals_And_HashCode_Vo {
     private final String str1 = "abc";
     private final String str2 = "abc";
