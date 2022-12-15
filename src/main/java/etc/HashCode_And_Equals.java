01bbb5377f702278a21516cf591dfeea3ecd047e
whale0928 <rlagusrl928@gmail.com>
2022-12-15 22:23:18 +0900

    chore : Directory Change

---
+++ src/main/java/etc/HashCode_And_Equals.java
@@ -0,0 +1,9 @@
+package etc;
+
+
+/**해시코드 , == , equals 들을 비교해보기 위한 클래스
+ * The type Hash code and equals.
+ */
+public class HashCode_And_Equals {
+    //new HashCode_And_Equals();
+}
---
+++ src/main/java/vo/For_Equals_And_HashCode_Vo.java
@@ -0,0 +1,8 @@
+package vo;
+
+
+public class For_Equals_And_HashCode_Vo {
+    private final String str1 = "abc";
+    private final String str2 = "abc";
+    private final String str3 = "abc";
+}
