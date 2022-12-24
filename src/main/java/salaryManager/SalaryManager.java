7c366f6fa95c13151e5a6d9c1c8702d5d17d4b29
whale0928 <rlagusrl928@gmail.com>
2022-12-24 19:50:54 +0900

    feat : YearSalary To Month
    -연봉을 월급으로 반환하는 메서드

--- src/main/java/salaryManager/SalaryManager.java
+++ src/main/java/salaryManager/SalaryManager.java
@@ -7,4 +7,14 @@ public class SalaryManager {
     public static void main(String[] args) {
 
     }
+
+    /**연봉을 제공받아 월급을 반환한다.
+     * Get monthly salary double.
+     *
+     * @param yearlySalary the yearly salary
+     * @return the double
+     */
+    public double getMonthlySalary(int yearlySalary){
+        return yearlySalary/12.0;
+    }
 }
