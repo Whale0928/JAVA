8a17460c319089cb5d8638be97d5407b084dcdaf
whale0928 <rlagusrl928@gmail.com>
2022-12-24 23:03:20 +0900

    feat : 일자에 따라 이율을 계산하는 메서드 , 365을 계산하는 메서드

--- src/main/java/DoitQuest/InterestManager.java
+++ src/main/java/DoitQuest/InterestManager.java
@@ -1,23 +1,44 @@
 package DoitQuest;
 
-/**은행에서 사용할 이율을 구하는 클랙스 구현 과제형문제
+/**
+ * 은행에서 사용할 이율을 구하는 클랙스 구현 과제형문제
  * The type Interst manager.
  */
 public class InterestManager {
-    public static void main(String[] args) {
+    final static int amount = 1_000_000;
+    final static InterestManager im = new InterestManager();
 
+    public static void main(String[] args) {
+        for (int i = 1; i <= 365; i++) {
+            double value = im.calculateAmount(i, amount);
+            System.out.printf("%d일 이자금액 : %.2f\n", i, value);
+        }
     }
 
-    /**일자를 매개변수로 받아 이율을 계산해 반환하는 메서드
+    /**
+     * 일자를 매개변수로 받아 이율을 계산해 반환하는 메서드
      * Get interes rate double.
      *
      * @param day the day
      * @return the double
      */
-    public double getInteresRate(int day){
-        if(day<=90) return  0.5;
-        else if(day<=180) return 1.0;
-        else if(day<=364) return  2.0;
-        else return  5.6;
+    public double getInterestRate(int day) {
+        if (day <= 90) return 0.005;
+        else if (day <= 180) return 0.010;
+        else if (day <= 364) return 0.020;
+        else return 0.056;
+    }
+
+    /**
+     * 예금 거치 기간과 금액을 받아 금액을 계산해 반환한다.
+     * Cacluate amout double.
+     *
+     * @param day    the day
+     * @param amount the amount
+     * @return the double
+     */
+    public double calculateAmount(int day, long amount) {
+        double interestRate = getInterestRate(day);
+        return amount * interestRate;
     }
 }
