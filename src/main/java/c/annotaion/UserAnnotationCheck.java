3e0a163576809d5de63ff3ee7e36ec8ab1bae413
whale0928 <rlagusrl928@gmail.com>
2023-01-08 23:35:44 +0900

    feat : Test Method Create

--- src/main/java/c/annotaion/UserAnnotationCheck.java
+++ src/main/java/c/annotaion/UserAnnotationCheck.java
@@ -1,23 +1,27 @@
 package c.annotaion;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.lang.reflect.Method;
 
 public class UserAnnotationCheck {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         UserAnnotationCheck check = new UserAnnotationCheck();
+        check.checkAnnotations(UserAnnotationSample.class);
     }
-    public void checkAnnotations(Class useClass){
+
+    public void checkAnnotations(@NotNull Class useClass) {
         Method[] methods = useClass.getDeclaredMethods();
 
-        for(Method method : methods){
+        for (Method method : methods) {
             UserAnnotation annotation = method.getAnnotation(UserAnnotation.class);
 
-            if (annotation != null){
+            if (annotation != null) {
                 int number = annotation.number();
                 String text = annotation.text();
-                System.out.println(method.getName()+"number = " + number+"text = " + text);
-            }else{
-                System.out.println(method.getName()+"is Null");
+                System.out.println(method.getName() + "   number = " + number + "     text = " + text);
+            } else {
+                System.out.println(method.getName() + "is Null");
             }
         }
     }
