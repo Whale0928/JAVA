859c03ab57fef5a77f5b84b6022883ba1da52926
whale0928 <rlagusrl928@gmail.com>
2023-01-11 20:12:08 +0900

    feat : 제네릭 상속 제한

---
+++ src/main/java/d/generic/CarWildcardSample.java
@@ -0,0 +1,18 @@
+package d.generic;
+
+public class CarWildcardSample {
+    public static void main(String[] args) {
+        CarWildcardSample sample = new CarWildcardSample();
+        sample.callBoundedWildcardMethod();
+    }
+
+    private void callBoundedWildcardMethod() {
+        WildcardGeneric<Car> wildcard = new WildcardGeneric<>();
+        wildcard.setWildcard(new Bus("3154번 버스"));
+        boundedWildcardMethod(wildcard);
+    }
+    public void boundedWildcardMethod(WildcardGeneric<? extends Car> c){
+        Car wildcard = c.getWildcard();
+        System.out.println(wildcard);
+    }
+}
